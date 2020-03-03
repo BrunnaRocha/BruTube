1a20943679d9ce947277b018570fbfe5aaa6e26e
Golias <joao.victor@mobile2you.com.br>
2020-03-03 15:14:06 -0300

    configuracao basica terminada

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,8 +1,18 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const app = express();
+const router = express.Router();
+
+router.get("/helloWorld", (req: express.Request, res: express.Response) => {
+  res.send({
+    message: "Hello from Firebase!"
+  });
+});
+
+app.use(cors()).use(router);
+
+export const brutubeApi = functions.https.onRequest(app);
